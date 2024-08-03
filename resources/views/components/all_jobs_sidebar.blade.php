0442b2b9f620460aba4633fcaee59ff04ad5ac7c
Nityasaha13 <nityasaha39@gmail.com>
2024-08-04 04:06:32 +0530

    ajax job search, jobs page

--- resources/views/components/all_jobs_sidebar.blade.php
+++ resources/views/components/all_jobs_sidebar.blade.php
@@ -1,65 +1,49 @@
 <div class="col-md-4 col-lg-3 sidebar mb-4">
-    <div class="card border-0 shadow p-4">
-        <div class="mb-4">
-            <h2>Keywords</h2>
-            <input type="text" placeholder="Keywords" class="form-control">
+    <form id="search-form">
+        <div class="card border-0 shadow p-4">
+            <div class="mb-4">
+                <h2>Keywords</h2>
+                <input type="text" id="keywords" name="keywords" placeholder="Keywords" class="form-control">
+            </div>
+
+            <div class="mb-4">
+                <h2>Location</h2>
+                <input type="text" id="location" name="location" placeholder="Location" class="form-control">
+            </div>
+
+            <div class="mb-4">
+                <h2>Category</h2>
+                <select name="category" id="category" class="form-control">
+                    <option value="">Select a Category</option>
+                    <option value="Engineering">Engineering</option>
+                    <option value="Accountant">Accountant</option>
+                    <option value="Information Technology">Information Technology</option>
+                    <option value="Fashion designing">Fashion Designing</option>
+                </select>
+            </div>                   
+
+            <div class="mb-4">
+                <h2>Job Type</h2>
+                <div class="form-check mb-2"> 
+                    <input class="form-check-input" name="job_type[]" type="checkbox" value="Full Time" id="full_time">    
+                    <label class="form-check-label" for="full_time">Full Time</label>
+                </div>
+
+                <div class="form-check mb-2"> 
+                    <input class="form-check-input" name="job_type[]" type="checkbox" value="Part Time" id="part_time">    
+                    <label class="form-check-label" for="part_time">Part Time</label>
+                </div>
+
+                <div class="form-check mb-2"> 
+                    <input class="form-check-input" name="job_type[]" type="checkbox" value="Freelance" id="freelance">    
+                    <label class="form-check-label" for="freelance">Freelance</label>
+                </div>
+
+                <div class="form-check mb-2"> 
+                    <input class="form-check-input" name="job_type[]" type="checkbox" value="Remote" id="remote">    
+                    <label class="form-check-label" for="remote">Remote</label>
+                </div>
+            </div>
         </div>
-
-        <div class="mb-4">
-            <h2>Location</h2>
-            <input type="text" placeholder="Location" class="form-control">
-        </div>
-
-        <div class="mb-4">
-            <h2>Category</h2>
-            <select name="category" id="category" class="form-control">
-                <option value="">Select a Category</option>
-                <option value="">Engineering</option>
-                <option value="">Accountant</option>
-                <option value="">Information Technology</option>
-                <option value="">Fashion designing</option>
-            </select>
-        </div>                   
-
-        <div class="mb-4">
-            <h2>Job Type</h2>
-            <div class="form-check mb-2"> 
-                <input class="form-check-input " name="job_type" type="checkbox" value="1" id="">    
-                <label class="form-check-label " for="">Full Time</label>
-            </div>
-
-            <div class="form-check mb-2"> 
-                <input class="form-check-input school-section" name="job_type" type="checkbox" value="1" id="">    
-                <label class="form-check-label " for="">Part Time</label>
-            </div>
-
-            <div class="form-check mb-2"> 
-                <input class="form-check-input school-section" name="job_type" type="checkbox" value="1" id="">    
-                <label class="form-check-label " for="">Freelance</label>
-            </div>
-
-            <div class="form-check mb-2"> 
-                <input class="form-check-input school-section" name="job_type" type="checkbox" value="1" id="">    
-                <label class="form-check-label " for="">Remote</label>
-            </div>
-        </div>
-
-        {{-- <div class="mb-4">
-            <h2>Experience</h2>
-            <select name="category" id="category" class="form-control">
-                <option value="">Select Experience</option>
-                <option value="">1 Year</option>
-                <option value="">2 Years</option>
-                <option value="">3 Years</option>
-                <option value="">4 Years</option>
-                <option value="">5 Years</option>
-                <option value="">6 Years</option>
-                <option value="">7 Years</option>
-                <option value="">8 Years</option>
-                <option value="">9 Years</option>
-                <option value="">10 Years</option>
-                <option value="">10+ Years</option>
-            </select>
-        </div> --}}
-    </div>
+    </form>
 </div>
