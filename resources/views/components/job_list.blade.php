d14834f80e8b015bc961e144818c44341bc411ce
Nityasaha13 <nityasaha39@gmail.com>
2024-08-17 00:12:41 +0530

    job cards equal height

--- resources/views/components/job_list.blade.php
+++ resources/views/components/job_list.blade.php
@@ -1,33 +1,34 @@
 <div class="row">
-    @forelse($jobs as $job)
-        <div class="col-md-4">
-            <div class="card border-0 p-3 shadow mb-4">
-                <div class="card-body">
-                    <h3 class="border-0 fs-5 pb-2 mb-0">{{ $job->role }}</h3>
-                    <p>{{ Str::words($job->description, 15, '...') }}</p>
-                    <div class="bg-light p-3 border">
-                        <p class="mb-0">
-                            <span class="fw-bolder"><i class="fa fa-map-marker"></i></span>
-                            <span class="ps-1">{{ $job->location }}</span>
-                        </p>
-                        <p class="mb-0">
-                            <span class="fw-bolder"><i class="fa fa-clock-o"></i></span>
-                            <span class="ps-1">{{ $job->job_type }}</span>
-                        </p>
-                        <p class="mb-0">
-                            <span class="fw-bolder"><i class="fa fa-inr"></i></span>
-                            <span class="ps-1">{{ $job->salary }}</span>
-                        </p>
-                    </div>
+    @foreach($jobs as $job)
+    <div class="col-md-4 d-flex align-items-stretch">
+        <div class="card border-0 p-3 shadow mb-4 w-100 d-flex flex-column">
+            <div class="card-body d-flex flex-column">
+                <h3 class="border-0 fs-5 pb-2 mb-0">{{$job->role}}</h3>
+                <p>{{ Str::words($job->description, 15, '...') }}</p>
+                <div class="bg-light p-3 border">
+                    <p class="mb-0">
+                        <span class="fw-bolder"><i class="fa fa-map-marker"></i></span>
+                        <span class="ps-1">{{$job->location}}</span>
+                    </p>
+                    <p class="mb-0">
+                        <span class="fw-bolder"><i class="fa fa-clock-o"></i></span>
+                        <span class="ps-1">{{$job->job_type}}</span>
+                    </p>
+                    <p class="mb-0">
+                        <span class="fw-bolder"><i class="fa fa-inr"></i></span>
+                        <span class="ps-1">{{$job->salary}}</span>
+                    </p>
+                </div>
+
+                <!-- Spacer div to push the button to the bottom -->
+                <div class="mt-auto">
                     <div class="d-grid mt-3">
-                        <a href="{{ route('single-job', $job->id) }}" class="btn btn-primary btn-lg">Details</a>
+                        <a href="{{route('single-job',$job->id)}}" class="btn btn-primary btn-lg">Details</a>
                     </div>
                 </div>
             </div>
         </div>
-    @empty
-        <div class="col-12">
-            <p>No jobs found.</p>
-        </div>
-    @endforelse
+    </div>
+    @endforeach                                                   
 </div>
+
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -71,7 +71,7 @@
                 @foreach($categories as $category)
                     <div class="col-lg-4 col-xl-3 col-md-6">
                         <div class="single_catagory">
-                            <a href="{{ route('search') }}">
+                            <a href="{{ route('jobs') }}">
                                 <h4 class="pb-2">{{ $category->category }}</h4>
                             </a>
                             <p class="mb-0"> <span>{{ $category->job_count }}</span> Available positions</p>
@@ -84,41 +84,11 @@
     
     {{-- <section class="section-3  py-5">
         <div class="container">
-            <h2>Featured Jobs</h2>
+            <h2>New Jobs</h2>
             <div class="row pt-5">
                 <div class="job_listing_area">                    
                     <div class="job_lists">
-                        <div class="row">
-                            @foreach($jobs as $job)
-                            <div class="col-md-4">
-                                <div class="card border-0 p-3 shadow mb-4">
-                                    <div class="card-body">
-                                        <h3 class="border-0 fs-5 pb-2 mb-0">{{$job->role}}</h3>
-                                        <p>{{ Str::words($job->description, 15, '...') }}</p>
-                                        <div class="bg-light p-3 border">
-                                            <p class="mb-0">
-                                                <span class="fw-bolder"><i class="fa fa-map-marker"></i></span>
-                                                <span class="ps-1">{{$job->location}}</span>
-                                            </p>
-                                            <p class="mb-0">
-                                                <span class="fw-bolder"><i class="fa fa-clock-o"></i></span>
-                                                <span class="ps-1">{{$job->job_type}}</span>
-                                            </p>
-                                            <p class="mb-0">
-                                                <span class="fw-bolder"><i class="fa fa-inr"></i></span>
-                                                <span class="ps-1">{{$job->salary}}</span>
-                                            </p>
-                                        </div>
-
-                                        <div class="d-grid mt-3">
-                                            <a href="{{route('single-job',$job->id)}}" class="btn btn-primary btn-lg">Details</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            @endforeach 
-                                                     
-                        </div>
+                        @include('components.job_list')
                     </div>
                 </div>
             </div>
@@ -127,40 +97,11 @@
     
     <section class="section-3 bg-2 py-5">
         <div class="container">
-            <h2>Latest Jobs</h2>
+            <h2>Featured Jobs</h2>
             <div class="row pt-5">
                 <div class="job_listing_area">                    
                     <div class="job_lists">
-                        <div class="row">
-                            @foreach($jobs as $job)
-                            <div class="col-md-4">
-                                <div class="card border-0 p-3 shadow mb-4">
-                                    <div class="card-body">
-                                        <h3 class="border-0 fs-5 pb-2 mb-0">{{$job->role}}</h3>
-                                        <p>{{ Str::words($job->description, 15, '...') }}</p>
-                                        <div class="bg-light p-3 border">
-                                            <p class="mb-0">
-                                                <span class="fw-bolder"><i class="fa fa-map-marker"></i></span>
-                                                <span class="ps-1">{{$job->location}}</span>
-                                            </p>
-                                            <p class="mb-0">
-                                                <span class="fw-bolder"><i class="fa fa-clock-o"></i></span>
-                                                <span class="ps-1">{{$job->job_type}}</span>
-                                            </p>
-                                            <p class="mb-0">
-                                                <span class="fw-bolder"><i class="fa fa-inr"></i></span>
-                                                <span class="ps-1">{{$job->salary}}</span>
-                                            </p>
-                                        </div>
-
-                                        <div class="d-grid mt-3">
-                                            <a href="{{route('single-job',$job->id)}}" class="btn btn-primary btn-lg">Details</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            @endforeach                                                   
-                        </div>
+                        @include('components.job_list')                       
                     </div>
                 </div>
             </div>
